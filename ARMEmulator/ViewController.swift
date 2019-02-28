6981bd3eb9e05e726b12c8bfb8e7f433a07f2630
Grant Savage <grantsavage1@me.com>
2019-02-27 23:18:31 -0700

    Initial Commit

---
+++ ARMEmulator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ARMEmulator
+//
+//  Created by Grant Savage on 2/27/19.
+//  Copyright © 2019 Grant Savage. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
